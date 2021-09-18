2c5ce3ce30eb69f150fe7557c2080c487df8b596
vaibhav.eric <vaibhav.eric@gmail.com>
2021-09-18 15:27:34 +0530

    test

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <div className="footer">
             <hr/>
-            <p>&copy; willjw3 {new Date().getFullYear()}</p>
+            <p>&copy; Vaibhav {new Date().getFullYear()}</p>
         </div>
     )
 }
